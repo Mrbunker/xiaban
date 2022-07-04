3e1922bdf2b5af976b02cbd5c8bc19f47eb6acd4
mrbunker <Mrbunker@qq.com>
2022-07-04 23:53:41 +0800

    update

--- scripts/upload.js
+++ scripts/upload.js
@@ -2,17 +2,17 @@ const fs = require("fs");
 const shell = require("shelljs");
 fs.rename("./docs", `./docsBuckup/docs_${new Date().getTime()}`, (err) => {
   if (err) throw err;
-  console.log("「docs buckup successfully」");
+  // console.log("「docs buckup successfully」");
 });
 
 fs.rename("./dist", "./docs", (err) => {
   if (err) throw err;
-  console.log("「dist renamed to docs successfully」");
+  // console.log("「dist renamed to docs successfully」");
 });
 
 fs.writeFile("./docs/CNAME", "mrbk.xyz", { flag: "a+" }, (err) => {
   // if (err) throw err;
-  console.log("「CNAME file created successfully」");
+  // console.log("「CNAME file created successfully」");
 });
 
 shell.exec("git add .");
@@ -20,4 +20,4 @@ const message = require("yargs").argv._[0];
 shell.exec(`git commit -m ${message}`);
 shell.exec("git pull");
 shell.exec("git push");
-console.log("「git commit successfully」");
+console.log("「upload successfully」");
