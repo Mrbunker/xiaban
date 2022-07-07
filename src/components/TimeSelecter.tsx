699b9ae306b34404385b455ec0cff7149224a762
mrbunker <Mrbunker@qq.com>
2022-07-08 00:57:50 +0800

    update

--- src/components/TimeSelecter.tsx
+++ src/components/TimeSelecter.tsx
@@ -1,8 +1,8 @@
 import moment from "moment";
 import styled from "styled-components";
 import { useEffect, useState } from "preact/hooks";
-import { Button, Radio, RadioChangeEvent, TimePicker } from "antd";
-import { getHHmmMoment } from "../tools/momentP";
+import { Button, TimePicker } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 import { titleType } from "../app";
 
@@ -17,18 +17,11 @@ export default function ({
   title: { content: string; ani: boolean };
   setTitleP: (newTitle: titleType) => void;
 }) {
-  const xiabanList = ["18:00", "17:30", "17:00"];
-  const [showSelecter, setShowSelecter] = useState(false);
   const [tx, setTx] = useState<"0%" | "-100%">("0%");
   useEffect(() => {
-    showSelecter && setShowSelecter(false);
     tx !== "0%" && setTx("0%");
     setTitleP({ content: `已设置为${moment(memoryTime).format("HH:mm")}`, ani: true });
   }, [memoryTime]);
-
-  const onRadioChange = ({ target: { value } }: RadioChangeEvent) => {
-    setMemoryTime(getHHmmMoment(value));
-  };
 
   const onPickerChange = (value: moment.Moment | null) => {
     if (value === null) return;
@@ -36,75 +29,67 @@ export default function ({
   };
 
   return (
-    <SelectWraper>
-      {!showSelecter ? (
+    <TranslateWraper tx={tx}>
+      <div className="translate">
         <div
-          // onMouseEnter={() => {
-          // tx !== "0%" && setTx("0%");
-          // setShowSelect(true);
-          // }}
+          className={`translate-btn highlight ${title.ani ? "right-in" : ""}`}
           title={"click to select xiaban time"}
-          onClick={() => !showSelecter && setShowSelecter(true)}
-          className={`highlight ${title.ani ? "right-in" : ""}`}
+          onClick={() => setTx("-100%")}
         >
           {title.content}
         </div>
-      ) : (
-        <TranslateWraper tx={tx}>
-          <div className="translate">
-            <div className="translate-radio">
-              <Radio.Group
-                options={xiabanList}
-                onChange={onRadioChange}
-                optionType="button"
-                defaultValue={moment(memoryTime).format("HH:mm")}
-                buttonStyle="solid"
-              />
-              <Button onClick={() => setTx("-100%")}>{"其他 >"}</Button>
-            </div>
-            <TimePicker
-              className="translate-picker"
-              onChange={onPickerChange}
-              format={"HH:mm"}
-              minuteStep={10}
-              disabledTime={() => {
-                /** 禁用0~14 */
-                return { disabledHours: () => Array.from({ length: 12 }, (_item, index) => index) };
-              }}
-              value={moment(memoryTime)}
-              hideDisabledOptions={true}
-            />
-          </div>
-        </TranslateWraper>
-      )}
-    </SelectWraper>
+
+        <div className="translate-picker">
+          <Button
+            onClick={() => setTx("0%")}
+            className="translate-picker-back"
+          >
+            {"<"}
+          </Button>
+          <TimePicker
+            onChange={onPickerChange}
+            format={"HH:mm"}
+            minuteStep={10}
+            disabledTime={() => {
+              /** 禁用0~16 */
+              return { disabledHours: () => Array.from({ length: 16 }, (_item, index) => index) };
+            }}
+            value={moment(memoryTime)}
+            hideDisabledOptions={true}
+          />
+        </div>
+      </div>
+      <></>
+    </TranslateWraper>
   );
 }
-
-const SelectWraper = styled.div`
-  margin-top: 6rem;
-  width: 60rem;
-  height: 12rem;
-  /* background: #F0F0F0; */
-  display: flex;
-  align-items: center;
-  font-size: 1.3rem;
-`;
 const TranslateWraper = styled.div<{ tx: string }>`
-  width: 30rem;
+  width: 25rem;
+  height: 5rem;
+  margin-top: 10rem;
+  font-size: 18px;
+  /* font-size: 1.3rem; */
+  /* min-font */
   overflow-x: hidden;
   .translate {
     display: flex;
+    align-items: center;
     transition: transform 0.75s;
     transform: translateX(${(props) => props.tx});
-    align-items: center;
 
-    &-picker,
-    &-radio {
+    &-picker {
+      flex-shrink: 0;
+      &-back {
+        line-height: 100%;
+        padding: 4px 10px;
+        color: #bfbfbf;
+      }
+    }
+    &-btn {
       flex-shrink: 0;
     }
-    &-radio {
-      width: 30rem;
+    &-btn {
+      width: 25rem;
     }
   }
 `;
